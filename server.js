4562c6c17c546d34bdde3fc725b1015a1640bff4
Damian Padilla <damipad4@gmail.com>
2023-01-30 23:53:11 -0600

    implemented mvc

--- server.js
+++ server.js
@@ -1,73 +1,79 @@
-const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 3000
+const connectDB = require('./config/databse')
+const homeRoutes = require('./routes/home')
 
-let dbName = 'clients'
+require('dotenv').config({path: './config/.env'})
 
-const dbConnectionStr = `mongodb+srv://dami:dami444@cluster0.hstkn5y.mongodb.net/?retryWrites=true&w=majority`
+connectDB()
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        const db = client.db('clients-database')
-        const clientCollection = db.collection('clients')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extend: true }))
+app.use(express.json())
 
-        //============
-        // Middlewares
-        //============
-        app.set('view engine', 'ejs')
-        app.use(express.static('public'))
-        app.use(bodyParser.urlencoded({ extend: true }))
-        app.use(express.json())
+app.use('/', homeRoutes)
 
-        //=======
-        // Routes
-        //=======
-
-        app.get('/', (req, res) => {
-            db.collection('clients').find().toArray()
-                .then(clients => {
-                    res.render('index.ejs', { clients: clients})
-                })
-                .catch(error => console.error(error))
-        })
-        
-        app.post('/addClient', (req, res) => {
-            clientCollection.insertOne({name: req.body.clientName, phone: req.body.clientPhone})
-                .then(result => {
-                    res.redirect('/')
-                })
-                .catch(error => console.error(error))
-        })
-
-        app.put('/editClient', (req, res) => {
-            console.log('editing...');
-            db.collection('clients').updateOne({name: req.body.name},     
-            {   
-                sort: {_id: -1},
-                upsert: false
-            })
-            .then(result => {
-                res.json('Success')
-                console.log('Client Deleted')
-            })
-            .catch(error => console.error(error))
-        })
-        
-        app.delete('/deleteClient', (req, res) => {
-            db.collection('clients').deleteOne({name: req.body.name})
-                .then(result => {
-                    console.log('Client Deleted')
-                    res.json('Deleted Client')
-                })
-                .catch(error => console.error(error))
-        })
-
-    })
-    .catch(console.error)
-
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server running on port ${process.env.PORT}`)
 })
+
+// const password = process.env.db_password
+
+// let dbName = 'clients'
+
+// const dbConnectionStr = `mongodb+srv://dami:${password}@cluster0.hstkn5y.mongodb.net/?retryWrites=true&w=majority`
+
+// MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+//     .then(client => {
+//         console.log(`Connected to ${dbName} Database`)
+//         const db = client.db('clients-database')
+//         const clientCollection = db.collection('clients')
+
+
+
+//         //=======
+//         // Routes
+//         //=======
+
+//         app.get('/', (req, res) => {
+//             db.collection('clients').find().toArray()
+//                 .then(clients => {
+//                     res.render('index.ejs', { clients: clients})
+//                 })
+//                 .catch(error => console.error(error))
+//         })
+        
+//         app.post('/addClient', (req, res) => {
+//             clientCollection.insertOne({name: req.body.clientName, phone: req.body.clientPhone})
+//                 .then(result => {
+//                     res.redirect('/')
+//                 })
+//                 .catch(error => console.error(error))
+//         })
+
+//         app.put('/editClient', (req, res) => {
+//             console.log('editing...');
+//             db.collection('clients').updateOne({name: req.body.name},     
+//             {   
+//                 sort: {_id: -1},
+//                 upsert: true
+//             })
+//             .then(result => {
+//                 res.json('Success')
+//                 console.log('Client Deleted')
+//             })
+//             .catch(error => console.error(error))
+//         })
+        
+//         app.delete('/deleteClient', (req, res) => {
+//             db.collection('clients').deleteOne({name: req.body.name})
+//                 .then(result => {
+//                     console.log('Client Deleted')
+//                     res.json('Deleted Client')
+//                 })
+//                 .catch(error => console.error(error))
+//         })
+
+//     })
+//     .catch(console.error)
